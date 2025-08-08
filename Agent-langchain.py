20a98f2e0afde95ffc87cf96a5ff9d55e3665fa0
Dhanush mudumby <buttowskikik33@gmail.com>
2025-08-08 11:58:08 +0530

    removed the code

--- Agent-langchain.py
+++ Agent-langchain.py
@@ -9,5 +9,3 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
-path = "C:/Users/dmudu/OneDrive/Desktop/MYProject/Demo-Project/Demo/app/HR_policies.txt"
-loader = TextLoader(path, encoding="utf-8")
